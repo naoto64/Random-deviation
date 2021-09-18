5a9f8824f4f51b1b2fe0fdb72aec3d82ba05d4e6
naoto64 <51907267+naoto64@users.noreply.github.com>
2021-09-19 01:13:35 +0900

    Add files via upload

---
+++ random.js
@@ -0,0 +1,38 @@
+function average(x) {
+    let n = x.length;
+    let avg = 0;
+
+    for(i=0; i<n; i++) {
+        avg += x[i];
+    }
+    return avg / n; //  (1 / n * avg)
+}
+let min = 1;
+function standardDeviation(x, avg) {
+    let n = x.length;
+    let sum = 0;
+    for (i=0; i<n; i++) {
+        sum+=Math.pow(x[i] - avg, 2);
+    }
+    return Math.sqrt(sum / n);
+}
+
+function loop() {
+  let x = [];
+  for (var i = 0; i < 10; i++) {
+    x[i] = Math.random();
+  }
+  let avg = average(x);
+  let sd = standardDeviation(x, avg);
+  if(sd < min){
+    min = sd;
+  }
+
+  document.body.textContent = "標準偏差: " + min;
+}
+
+function main() {
+    window.setInterval(loop, 1);
+}
+
+window.addEventListener("load", main, false);
